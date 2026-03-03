cbd401b1a812a625db9d5d5b54a9f3e069e49975
OleksiiKar <oleksiykar@icloud.com>
2026-03-03 19:35:07 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Main {
     public static int defineVowels(String word){
-        String vowels = word.replaceAll("[бпвмфдтзсцлнржшчгкхґй]", "");
+        String vowels = word.replaceAll("[^аеиіоуяєюї]", "");
         return vowels.length();
     }
     public static void main(String[] args){
