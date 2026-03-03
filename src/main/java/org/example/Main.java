a4710dd352e8733f3255a1bf662d75a70d7726dd
OleksiiKar <oleksiykar@icloud.com>
2026-03-03 20:27:46 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,18 +13,22 @@ public class Main {
         String vowels = word.replaceAll("[^аеиіоуяєюї]", "");
         return vowels.length();
     }
-    public static void main(String[] args){
-        StringBuilder text = new StringBuilder();
-        text.append("Привіт, як справи у тебе?");
-        System.out.println(text);
+    public static StringBuilder clearText(StringBuilder text){
         Pattern pattern = Pattern.compile("[!?.,]");
         Matcher matcher = pattern.matcher(text);
         StringBuilder result = new StringBuilder();
         while (matcher.find()){
             matcher.appendReplacement(result, "");
         }
-        pattern = Pattern.compile("\\s+");
-        List<String> data = new ArrayList<>(Arrays.asList(pattern.split(result)));
+        return result;
+    }
+    public static void main(String[] args){
+        StringBuilder text = new StringBuilder();
+        text.append("Привіт, як справи у тебе?");
+        System.out.println(text);
+        text = clearText(text);
+        Pattern pattern = Pattern.compile("\\s+");
+        List<String> data = new ArrayList<>(Arrays.asList(pattern.split(text)));
         data.sort(Comparator.comparingInt(Main::defineVowels));
         text.setLength(0);
         for (String word:data){
