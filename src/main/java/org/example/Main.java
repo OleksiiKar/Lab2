4c55ee53a4277feaaa0bfd4310eee2c5435378dc
OleksiiKar <oleksiykar@icloud.com>
2026-03-03 18:06:54 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,11 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-
+        StringBuilder text = new StringBuilder();
+        text.append("Привіт, як справи у тебе?");
+        String text_str = text.toString();
+        text_str = text_str.replaceAll("[!?.,]", "").toLowerCase();
+        System.out.println(text_str);
     }
 }
 
