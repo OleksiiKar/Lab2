fad2511863700f914fa4be9319b20440435f19e8
OleksiiKar <oleksiykar@icloud.com>
2026-03-03 20:31:21 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,13 +22,17 @@ public class Main {
         }
         return result;
     }
+    public static List<String> prepareForCompare(StringBuilder text){
+        Pattern pattern = Pattern.compile("\\s+");
+        List<String> data = new ArrayList<>(Arrays.asList(pattern.split(text)));
+        return data;
+    }
     public static void main(String[] args){
         StringBuilder text = new StringBuilder();
         text.append("Привіт, як справи у тебе?");
         System.out.println(text);
         text = clearText(text);
-        Pattern pattern = Pattern.compile("\\s+");
-        List<String> data = new ArrayList<>(Arrays.asList(pattern.split(text)));
+        List<String> data = prepareForCompare(text);
         data.sort(Comparator.comparingInt(Main::defineVowels));
         text.setLength(0);
         for (String word:data){
