f05ef18ea78c5ddbeaf5521461f0b7e7e985f585
OleksiiKar <oleksiykar@icloud.com>
2026-03-04 23:28:01 +0200

    ReadMe

--- src/main/java/Lab2/Main.java
+++ src/main/java/Lab2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
         TextProcessing textProcessing = new TextProcessing();
         StringBuilder text = new StringBuilder();
-        text.append("кіт мак лід");
+        text.append("Привіт, як справи у тебе?");
         System.out.println(text);
         System.out.println(textProcessing.sortText(text));
     }
